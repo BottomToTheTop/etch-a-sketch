eeb63fbc751406743bd67728224420b2a7656825
BottomToTheTop <121840768+BottomToTheTop@users.noreply.github.com>
2025-05-05 12:20:02 +0100

    Implement logic that allows user to change grid size

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+button = document.querySelector(".popup");
 container = document.querySelector(".container");
 
 for (let i = 1; i <= 256; i++) {
@@ -6,7 +7,7 @@ for (let i = 1; i <= 256; i++) {
     div.classList.add("square");
 }
 
-squares = document.querySelectorAll('.container > div');
+let squares = document.querySelectorAll('.container > div');
 
 squares.forEach((x) => {
     x.addEventListener("mouseenter", (e) => {
@@ -14,8 +15,33 @@ squares.forEach((x) => {
     });
 });
 
-button = document.querySelector(".popup");
+
+function newGrid(number) {
+    for (let i = 1; i <= number*number; i++) {
+        div = document.createElement("div");
+        div.classList.add("square");
+        div.style.width = (800/number) + 'px'
+        div.style.height = (800/number) + 'px'
+        container.appendChild(div);
+    }
+
+    squares = document.querySelectorAll('.container > div');
+
+    squares.forEach((x) => {
+        x.addEventListener("mouseenter", (e) => {
+            x.style.backgroundColor = 'red';
+        });
+    });
+}
+
+function removeGrid() {
+    for (const square of squares) {
+        square.remove();
+    }
+}
 
 button.addEventListener("click", () => {
     let numberOfSquares = prompt("Please enter a number of squares for your grid:")
+    removeGrid();
+    newGrid(numberOfSquares);
 });
